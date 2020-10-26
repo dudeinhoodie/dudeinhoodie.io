71345362e7f79ef4bcd6d611010deb5111508e9f
OliaSh549 <oly549@mail.ru>
2020-10-26 20:53:09 +0300

    feat: add logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,13 @@ import { Link } from 'gatsby';
 
 import SEO from '../components/seo';
 import Layout from '../components/layout';
+import Logo from '../components/Logo';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
+    <Logo /> 
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
